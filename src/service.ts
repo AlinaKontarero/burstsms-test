299dedba8de9e70c3b3a7e6d46ba4c322b696ee9
alina-kontarero <alina.kontarero@gmail.com>
2020-09-08 10:34:25 +1000

    server updates

--- src/service.ts
+++ src/service.ts
@@ -1,8 +1,33 @@
+import authData from './config'
+
 type SendSMSResponse = void;
 
 export const sendSMS = (
   sender: string,
   recipient: string,
   message: string
-): SendSMSResponse => {};
+): SendSMSResponse => {
+const myHeaders = new Headers();
+myHeaders.append("x-api-key", authData.authId);
+myHeaders.append("Content-Type", "application/json");
 
+const raw = JSON.stringify({
+  message: message,
+  sender: sender,
+  recipient: recipient
+});
+
+const requestOptions: RequestInit = {
+  method: 'POST',
+  headers: myHeaders,
+  redirect: 'follow',
+  body: raw
+};
+
+const  proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+const  targetUrl = 'https://api.transmitmessage.com/v1/sms/message'
+
+fetch(proxyUrl + targetUrl, requestOptions)
+  .then(response => response.text())
+	.then(result => console.log(result))
+}
