06bb0658a3df3322ada9bfcd611723a58eaa72c1
alina-kontarero <alina.kontarero@gmail.com>
2020-09-08 10:37:44 +1000

    MessagePage: unbox response

--- src/MessagePage.tsx
+++ src/MessagePage.tsx
@@ -1,15 +1,44 @@
 import React from "react";
 
-import MessageDisplay from "./MessageDisplay";
-import MessageForm from "./MessageForm";
+import MessageDisplay, { IMessage } from "./MessageDisplay";
+import MessageForm, { IMessageData } from "./MessageForm";
+import { sendSMS } from "./service";
 
-const MessagePage = () => {
+interface State {
+  messages?: IMessage[]
+}
+
+
+class MessagePage extends React.Component<{}, State> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  constructor(props: any) {
+    super(props)
+    this.state = { messages: undefined }
+  }
+ private myCallback = async (messageData: IMessageData) => {
+    // const resp = 
+    await sendSMS(messageData.sender, messageData.recipient, messageData.message)
+    this.setState({messages: this.messages})
+  }  
+
+  messages: IMessage[] = [
+    {recipient: 'Alina', message: 'Hi how are you?', cost: 0.15},
+    {recipient: 'Alex', message: 'Here is a simple test of your understanding of React components', cost: 0.20}
+  ]
+  public render() {
+
+
   return (
     <div className="MessagePage">
-      <MessageForm />
-      <MessageDisplay />
+      <MessageForm 
+        callbackFromParent={this.myCallback}
+      />
+      <MessageDisplay
+        messages={this.state.messages}
+      />
     </div>
   );
+}
 };
 
 export default MessagePage;
